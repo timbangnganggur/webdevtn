c76b409dc2b4b2b2daedaf547407a53d08e679b5
alendro25 <57282213+alendro25@users.noreply.github.com>
2021-02-17 21:24:46 +0700

    Upload Ke-Lima
    Revisi ProfileTN

--- resources/views/general/profileTN.blade.php
+++ resources/views/general/profileTN.blade.php
@@ -1,18 +1,55 @@
 @extends('general.master')
 @section('content')
 
-	 <section class="sectionLogo">
-          <img src="/img/Logo.png" alt="">
-      </section>
+	 
+      <div class="container profileTN">
+        <section class="sectionLogo">
+          <div class="row">
+            <div class="col-md-6">
+              <img src="/img/Logo.png" alt="">
+            </div>
+            <div class="col-md-6">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, dicta? Vel, excepturi eligendi culpa impedit nemo ipsam, enim repudiandae in deleniti voluptatibus reprehenderit itaque, laboriosam saepe consequatur voluptatum necessitatibus voluptatem sint magnam. Similique, laborum repellat incidunt possimus corporis tempora ea, ad eligendi beatae adipisci deserunt maiores officiis amet! Sapiente quam ab iste minima possimus dolor reiciendis ea, ratione ex molestias labore! Sed omnis blanditiis veniam libero minus autem aspernatur. Non.</p>
+            </div>
+          </div>
+        </section>
 
-      <section class="section1 animate__animated animate__fadeInLeft animate__slow">
-        <img src="/img/PenjelasanTN.png" alt="">
+        <div class="section1">
+          <div class="row">
+            <div class="col-md-6">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, dicta? Vel, excepturi eligendi culpa impedit nemo ipsam, enim repudiandae in deleniti voluptatibus reprehenderit itaque, laboriosam saepe consequatur voluptatum necessitatibus voluptatem sint magnam. Similique, laborum repellat incidunt possimus corporis tempora ea, ad eligendi beatae adipisci deserunt maiores officiis amet! Sapiente quam ab iste minima possimus dolor reiciendis ea, ratione ex molestias labore! Sed omnis blanditiis veniam libero minus autem aspernatur. Non.</p>
+            </div>
+            <div class="col-md-6">
+              <img src="/img/ikon visi misi.png" alt="">
+            </div>
+          </div>
+        </div>
 
-      </section>
+        <h1>PRODUCT</h1>
 
-      <section class="section2 animate__animated animate__slideInRight animate__slower">
-          <img src="/img/VisiMisiTN.png" alt="">
-      </section>
+          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="/img/ikon Ayo Makryo.png" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="/img/gambar di ikon CLBK.png" class="d-block w-100" alt="...">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></i></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        
+
+        
+      </div><!--Akhir Container-->
+      
 
 
 
