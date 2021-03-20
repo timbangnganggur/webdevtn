6e6451ee3c253149cd7e03dacd65cb57c376ca00
alendro25 <57282213+alendro25@users.noreply.github.com>
2021-03-20 08:33:31 +0700

    Fix CLBK tag di Landing page

--- resources/views/general/landingpage.blade.php
+++ resources/views/general/landingpage.blade.php
@@ -3,28 +3,28 @@
 <div class="jumbotron jumbotron-fluid" style="background-image: url('{{ asset('img/jumbotron1.png') }}');">
   <div class="container">
     <br><br><br> <br><br><br> <br><br><br>
-      <div class="pencarian">
-          <div class="row d-flex justify-content-center">
-            <input class="col-lg-9 col-md-10 mx-1 my-1 form-block cari" type="search" placeholder="Cari Lowongan Kerja" aria-label="Search">
-            <button class="col-lg-1 col-md-10 mx-1 my-1 btn btn-outline-success" type="submit"><i class="fas fa-search"></i></button>  
-          </div>
-        </div>
+    <div class="pencarian">
+      <div class="row d-flex justify-content-center">
+        <input class="col-lg-9 col-md-10 mx-1 my-1 form-block cari" type="search" placeholder="Cari Lowongan Kerja" aria-label="Search">
+        <button class="col-lg-1 col-md-10 mx-1 my-1 btn btn-outline-success" type="submit"><i class="fas fa-search"></i></button>
       </div>
+    </div>
+  </div>
 </div>
 <!--Akhir Hero-->
 
 <section id="ayo-sinau" style="height: 100%">
-    <div class="row m-0 p-0 text-center">
-      <div class="col-lg-12 m-0 p-0">
-        <h2>
-          <b>CLBK</b>
-          <span style="font-size: 12pt">: CANGKRUK LANGSUNG BAHAS KERJOAN</span>
-        </h2>
-      </div>
-      <div class="col-lg-12 m-0 p-0">
-        <hr class="bg-dark mb-4 mt-0 d-inline-block mx-auto" style="width: 30%; height: 2px;">
-      </div>
+  <div class="row m-0 p-0 text-center">
+    <div class="col-lg-12 m-0 p-0 clbk-tag">
+      <h2>
+        <b>CLBK</b>
+      </h2>
+      <span style="font-size: 12pt">: CANGKRUK LANGSUNG BAHAS KERJOAN</span>
     </div>
+    <div class="col-lg-12 m-0 p-0">
+      <hr class="bg-dark mb-4 mt-0 d-inline-block mx-auto" style="width: 30%; height: 2px;">
+    </div>
+  </div>
 
   <div class="owl-carousel owl-theme">
     <div class="item"><img class="gambar-carousel" src="img/Tips.png" alt="">
