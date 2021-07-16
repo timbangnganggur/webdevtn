ffb20a8f6d60fc14e28a80036664c5c8687ce047
supriwahyu <supriwahyu95@gmail.com>
2021-07-16 23:15:03 +0700

    Menambahkan Navigasi

---
+++ app/Http/Controllers/Admin/RoleAdminController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RoleAdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $admins = Admin::all();
+        return view('admin.role.index', compact('admins'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.role.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $admin = new Admin();
+        $admin->name = $request->name;
+        $admin->username = $request->username;
+        $admin->username = $request->password;
+        $admin->password = Hash::make($request->password);
+        $admin->role = $request->role;
+        $admin->save();
+        return redirect('admin/role')->with('success', 'Artikel berhasil terbuat');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $admin = Admin::find($id);
+        if($admin->role != 'superadmin'){
+            $admin->delete();
+            return back()->with('success', 'Role berhasil terhapus');
+        }else{
+            return back()->with('error', 'Tidak dapat menghapus role ini');
+        }
+    }
+}
---
+++ resources/views/admin/role/create.blade.php
@@ -0,0 +1,90 @@
+@extends('admin.main')
+@section('style')
+    <link rel="stylesheet" href="{{ asset('./plugins/select2/css/select2.min.css') }}">
+@endsection
+@section('title')
+    Role
+@endsection
+@section('content')
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1>Role</h1>
+            </div>
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active">Role</li>
+              </ol>
+            </div>
+          </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12">
+                <form action="{{ route('admin.role.store') }}" method="POST">
+                    @csrf
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Tambah Data Role Baru</h3>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="name">Nama Admin</label>
+                                        <input class="form-control" type="text" name="name" id="name" placeholder="Masukkan nama admin"><br>        
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="name">Role Admin</label>
+                                        <select class="form-control" name="role">
+                                            <option value="artikel">Admin Artikel</option>
+                                            <option value="artikel">Admin Loker</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="username">Username Admin</label>
+                                        <input class="form-control" type="text" name="username" id="username" placeholder="Masukkan username admin"><br>        
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="password">Password Admin</label>
+                                        <input class="form-control" type="password" name="password" id="password" placeholder="Masukkan password admin"><br>        
+                                    </div>
+                                </div>
+                            </div>
+                        <div class="card-footer">
+                            <a href="{{ URL::to('admin/role') }}" class="btn btn-outline-info">Kembali</a>
+                            <button class="btn btn-primary">Simpan</button>
+                        </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+          </div>
+        </div>
+    </section>
+</div>
+@endsection
+@section('script')
+<script src="{{ asset('./plugins/select2/js/select2.full.min.js') }}"></script>
+<script>
+  $(function () {
+    //Initialize Select2 Elements
+    $('.select2').select2()
+  })
+</script>
+@endsection
