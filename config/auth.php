019d9132d1361332285eebde9d36e9dadc65f75f
Fitria Melliyanni <fitria.melliyanni@gmail.com>
2021-09-15 19:11:23 +0700

    Memperbarui AuthController dan file auth di folder config untuk function login

--- config/auth.php
+++ config/auth.php
@@ -51,6 +51,10 @@ return [
             'driver' => 'session',
             'provider' => 'admin',
         ],
+        'user' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
     ],
 
     /*
