7398e241e5536e0de5c2c0dd9b43366463c9b667
windugi <windugigih@gmail.com>
2021-09-16 11:05:23 +0700

    perbaiki tampilan dashboard supri punya

--- app/Http/Controllers/Admin/DashboardAdminController.php
+++ app/Http/Controllers/Admin/DashboardAdminController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Article;
+use App\Models\Job;
+
 
 class DashboardAdminController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $articles = Article::all();
+        $jobs = Job::all();
+        return view('admin.dashboard', compact('articles', 'jobs'));
     }
 }
