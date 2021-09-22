aa4f2ccbc2e4e49b26af9b8bfb3beb33d24d2f18
bryanrinaldoo <bryan.rinaldo24@gmail.com>
2021-09-22 09:50:23 +0700

    jelajah artikel done

--- resources/views/general/landingpage.blade.php
+++ resources/views/general/landingpage.blade.php
@@ -12,8 +12,8 @@ Timbangnganggur | Website Informatif & Edukatif untuk Persiapan Diri di Dunia Ke
 
         <form action="{{ route('ayoMakaryo') }}" method="GET">
             <div class="pencarian">
-                <input class="form-control" type="search"
-                    placeholder="Cari Lowongan Kerja" aria-label="Search" name="q">
+                <input class="form-control" type="search" placeholder="Cari Lowongan Kerja" aria-label="Search"
+                    name="q">
                 <button class="btn-search" type="submit"><i class="fas fa-search"></i> <span>Cari</span></button>
             </div>
         </form>
@@ -22,37 +22,76 @@ Timbangnganggur | Website Informatif & Edukatif untuk Persiapan Diri di Dunia Ke
 <!--Akhir Hero-->
 
 <section id="main">
-    <div class="card mx-2 my-4" style="border: none;">
-        <div class="card-body" style="background-color: rgba(244, 241, 222, 1);">
-            <h5 class="card-title text-center">Artikel</h5>
-            <h6 class="card-subtitle mb-2 text-muted text-center">Artikel</h6>
-            <div id="splide-artikel" class="splide">
-                <div class="splide__track d-flex">
-                    <ul class="splide__list">
-                        @foreach($articles as $article)
-                        <li class="splide__slide" style="min-width:250px;">
-                            <div class="card m-3 h-100">
-                                <img src="{{ asset('images/artikel/'.$article->image_url)}}" class="img-thumbnail">
-                                <div class="card-body">
-                                    <p class="card-text"><a
-                                            href="{{ route('artikel.show', ['artikel' => $article->slug]) }}">{{ $article->title }}</a>
-                                    </p>
-                                    <p>by ({{ $article->writer }})</p>
-                                    <p class="card-text"><small class="text-muted">{{ $article->created_at }}</small>
-                                    </p>
-                                    <p>Views: {{ $article->view_count }}</p>
-                                </div>
+
+    <!-- JELAJAHI ARTIKEL  -->
+    <div class="jelajah-artikel">
+        <h5 class="judul-section text-center">Jelajahi Artikel</h5>
+        <h6 class="subjudul-section text-center">Dapatkan Inspirasi untuk mengembangkan karirmu</h6>
+        <div class="card-body">
+            <a href="">
+                <div class="card">
+                    <img class="card-img-top" src="../img/berita.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Berita seputar dunia terupdate</h5>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                    <img class="card-img-top" src="../img/cv-hack.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">CV Hack</h5>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                    <img class="card-img-top" src="../img/interview.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Interview Skill</h5>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                    <img class="card-img-top" src="../img/artikel/konten.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Konten bermanfaat</h5>
+                    </div>
+                </div>
+            </a>
+            
+
+        </div>
+    </div>
+
+    <!-- ARTIKEL POPULER  -->
+    <div class="artikel-populer card-body">
+        <h5 class="judul-section text-center">Artikel</h5>
+        <h6 class="subjudul-section text-center">Artikel</h6>
+        <div id="splide-artikel" class="splide">
+            <div class="splide__track d-flex">
+                <ul class="splide__list">
+                    @foreach($articles as $article)
+                    <li class="splide__slide" style="min-width:250px;">
+                        <div class="card m-3 h-100">
+                            <img src="{{ asset('images/artikel/'.$article->image_url)}}" class="img-thumbnail">
+                            <div class="card-body">
+                                <p class="card-text"><a
+                                        href="{{ route('artikel.show', ['artikel' => $article->slug]) }}">{{ $article->title }}</a>
+                                </p>
+                                <p>by ({{ $article->writer }})</p>
+                                <p class="card-text"><small class="text-muted">{{ $article->created_at }}</small>
+                                </p>
+                                <p>Views: {{ $article->view_count }}</p>
                             </div>
-                        </li>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-            <div class="splide__progress">
-                <div class="splide__progress__bar">
-                </div>
+                        </div>
+                    </li>
+                    @endforeach
+                </ul>
             </div>
         </div>
+    </div>
     </div>
 </section>
 @endsection
@@ -69,7 +108,7 @@ Timbangnganggur | Website Informatif & Edukatif untuk Persiapan Diri di Dunia Ke
     } else {
         new Splide('#splide-artikel', {
             type: 'loop',
-            perPage: 5,
+            perPage: 3,
             perMove: 1,
             autoplay: true,
         }).mount();
