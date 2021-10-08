c64756819704ff9d9233f5265683689606ff4880
Fitria Melliyanni <fitria.melliyanni@gmail.com>
2021-10-08 21:13:52 +0700

    Membuat table Company_user untuk registrasi company baru

---
+++ database/migrations/2021_10_08_140014_create_company_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompanyUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name');
+            $table->string('username');
+            $table->string('email');
+            $table->string('password');
+            $table->text('company_logo');
+            $table->string('phone');
+            $table->text('company_bio');
+            $table->string('website');
+            $table->text('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company_users');
+    }
+}
